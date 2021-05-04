68aa02707dfcda181a766fffb9b70300866c318d
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-05-04 11:43:37 +0600

    skills done

--- src/components/About/AboutComponents/Skills.js
+++ src/components/About/AboutComponents/Skills.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import ProgressBar from "@ramonak/react-progress-bar";
 
 const Skills = () => {
     return (
-        <div>
-            <h2>skills</h2>
+        <div className='mt-5 p-5'>
+            <h2 style={{ color: 'white' }} className='text-center'>MY SKILLS</h2>
+            <h5>HTML: <ProgressBar completed={85} /></h5>
+            <h5>CSS: <ProgressBar completed={80} /></h5>
+            <h5>JavaScript: <ProgressBar completed={70} /></h5>
+            <h5>React: <ProgressBar completed={70} /></h5>
+            <h5>NodeJs: <ProgressBar completed={68} /></h5>
+            <h5>MongoDB: <ProgressBar completed={70} /></h5>
         </div>
     );
 };
