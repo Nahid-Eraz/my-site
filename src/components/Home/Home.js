90c6ff3c0a09455b7544d6e7676e221e9a347b49
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-05-04 10:33:40 +0600

    home done button left

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Home.css';
+import myImg from '../../img/me.jpg';
 
 const Home = () => {
     return (
         <div>
-            <h2>home</h2>
+            <div className="m-3  text-center">
+                <div className="row d-flex align-items-center justify-content-center">
+                    <div className="col-md-6 p-5">
+                        <img className='img-fluid' src={myImg} alt="" />
+                    </div>
+                    <div className="col-md-5">
+                        <h1 style={{ color: 'white' }} className="heading-text"><b>- I'm <span className='text-dark'>Nahid Eraz</span></b></h1>
+                        <h1 style={{ color: 'white' }}><b><span className='text-dark'>Front-End</span></b> Web Developer</h1>
+                        <h5 className="text-secondary">Focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</h5>
+
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
